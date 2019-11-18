bd7d4e0514d1dd0a7ddf6d0d1d012c55bba0781e
marilov <sokyrko.marina@gmail.com>
2019-11-18 23:41:15 +0200

    System print task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("          РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                          О. Хайям");
     }
 }
