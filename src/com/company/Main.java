e53b452e675c9bf789051a8c071a4eb84c87aea0
marilov <sokyrko.marina@gmail.com>
2019-11-18 23:59:23 +0200

    System print task - call from main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("          РУБАИ");
-        System.out.println("Много лет размышлял я над жизнью земной.");
-        System.out.println("Непонятного нет для меня под луной.");
-        System.out.println("Мне известно, что мне ничего не известно!");
-        System.out.println("Вот последняя правда, открытая мной.");
-        System.out.println("                          О. Хайям");
+        LessonSOP lessonSOP = new LessonSOP();
+        lessonSOP.rubai();
     }
 }
